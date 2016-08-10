65cff53e68fb3ddcc9a5d088129b942346046469
daveikaye <daveikaye@yahoo.com>
2016-08-09 20:02:52 -0400

    Convert I to arabic

--- src/roman_numerals.h
+++ src/roman_numerals.h
@@ -2,8 +2,5 @@
 #define ROMAN_NUMERALS_H
 
 int to_arabic(char *roman);
-char *to_roman(int arabic);
-char* add(char* number1, char* number2);
-char* subtract(char* number1, char* number2);
 
 #endif /* ROMAN_NUMERALS_H */
